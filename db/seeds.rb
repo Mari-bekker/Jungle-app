807c72f7ef44352c87593e81b8f52f327223fdfc
Marina Bekker <mshooch@gmail.com>
2022-07-09 17:24:58 -0400

    disabled add button when an item is sold out

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,7 +47,7 @@ cat1.products.create!({
   They rely on animal pollination to reproduce. To promote pollination, they produce aromatic scents.",
   image: open_asset('plante_1.jpg'),
   quantity: 0,
-  price: 64.99
+  price: 55.5
 })
 
 cat1.products.create!({
@@ -213,7 +213,7 @@ cat3.products.create!({
   As a defense mechanism the Cliff Collard grows thick thorns.
   They rely on winds to carry their seeds away to reproduce. Once pollinated, they grow small, inedible fruits.",
   image: open_asset('plante_12.jpg'),
-  quantity: 23,
+  quantity: 0,
   price: 79.99
 })
 
